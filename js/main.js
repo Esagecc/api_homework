9fb1f6ae8444a872acd60eb3a1ae5f34c1e2183f
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-07-25 00:55:10 -0500

    updated file to include video and updated css file

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 
-let date = document.querySelector('input.value')
 let button = document.querySelector('button')
 
 button.addEventListener('click', deliverData)
 
 
 function deliverData() {
-    fetch("https://api.nasa.gov/planetary/apod?api_key=aNrjLlX05MhPRubimfhzUpYv9Ur0oQg3fHbkUG02") 
+    const choice = document.querySelector('input').value
+    console.log(choice)
+
+    const url = `https://api.nasa.gov/planetary/apod?api_key=aNrjLlX05MhPRubimfhzUpYv9Ur0oQg3fHbkUG02&date=${choice}`
+    fetch(url) 
 .then (res => res.json())
 .then (data =>{
-    console.log(data)
-    console.log(data.hdurl)
+if ( data.media_type === 'image' ){
     document.querySelector('img').src = data.hdurl
-    console.log(data.title)
+    document.querySelector('img').style.border = '5px solid rgb(228, 65, 6)'
+} else if ( data.media_type === 'video' ) {
+    document.querySelector('iframe').src = data.url
+    document.querySelector('iframe').style.height = '500px'
+}
     document.getElementById('name').innerText = data.title
-    console.log(data.explanation)
     document.getElementById('des').innerText = data.explanation
 
 } )
